a2ff5c6221ea5afefd642f06b00effce016cce28
TJ <yj_ykt@icloud.com>
2023-01-19 09:58:16 +0900

    update: unit tests for TimerViewModel

--- authenticatorTests/authenticatorTests.swift
+++ authenticatorTests/authenticatorTests.swift
@@ -18,12 +18,20 @@ final class authenticatorTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testTimer() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        let timerViewModel = TimerViewModel()
+        let start = timerViewModel.model.remainingTime
+        for _ in 0..<30 {
+            timerViewModel.model.decrement()
+        }
+        let result = timerViewModel.model.remainingTime
+        XCTAssertEqual(result, start)
     }
 
     func testPerformanceExample() throws {
