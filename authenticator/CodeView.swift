928e499b883e827f6ba2474338d5aaa924c8fc97
TJ <yj_ykt@icloud.com>
2023-01-21 18:30:38 +0900

    update: code view styles

--- authenticator/CodeView.swift
+++ authenticator/CodeView.swift
@@ -13,10 +13,57 @@ struct CodeView: View {
     @ObservedObject
     var timerModel: TimerViewModel
     
+    @Binding
+    var codes: Array<String>
+    
     var body: some View {
         HStack {
-            Text("Code View")
-            Text("\(timerModel.model.remainingTime)")
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Code View")
+                            .font(.headline)
+                        Text(verbatim: "name@example.com")
+                            .font(.body)
+                            .foregroundColor(Color(.secondaryLabel))
+                    }
+                    Spacer()
+                    Text("\(timerModel.model.remainingTime)")
+                }
+                Spacer().frame(height: 15)
+                HStack {
+                    ForEach(codes, id: \.self) { c in
+                        Text(c)
+                            .font(Font.system(size: 30))
+                            .frame(width: 40, height: 50)
+                            .foregroundColor(Color(.highlightText))
+                            .background(Color(.codeBackground))
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                }
+            }
+            .padding(.all, 20)
+            .background(Color(.contentBackground))
+            .cornerRadius(20)
         }
+        .padding(.horizontal, 15)
+        .padding(.top, 10)
+    }
+}
+
+struct CodeView_Previews: PreviewProvider {
+    @ObservedObject static
+    var timerModel = TimerViewModel()
+    
+    @State static
+    var codes = ["1", "2", "3", "4", "5", "6"]
+    
+    static var previews: some View {
+        ScrollView {
+            CodeView(timerModel: timerModel, codes: $codes)
+            CodeView(timerModel: timerModel, codes: $codes)
+            Spacer()
+        }.background(Color(.background))
     }
 }
