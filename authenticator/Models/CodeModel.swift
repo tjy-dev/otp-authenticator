bb90134ef5eda8be2e1f94118458f38dd081ba6d
TJ <yj_ykt@icloud.com>
2023-01-23 09:17:16 +0900

    add: otp code model for data separation

--- authenticator/Models/CodeModel.swift
+++ authenticator/Models/CodeModel.swift
@@ -10,6 +10,38 @@ import CoreData
 import SwiftBase32
 import CryptoSwift
 
+struct OtpCodeModel {
+    
+    static
+    func computeCode (key: String) -> String {
+        guard let key = base32Decode(key.replacingOccurrences(of: " ", with: "")) else {
+            return "Error!"
+        }
+        
+        let intervalTime = 30
+        let date = Date.now
+        let unixTime: TimeInterval = date.timeIntervalSince1970
+        let unixTimeInt = Int(unixTime)
+        let timeSteps = unixTimeInt / intervalTime
+        
+        let counter = withUnsafeBytes(of: timeSteps.bigEndian, Array.init)
+        
+        let hash = try! HMAC(key: key, variant: .sha1).authenticate(counter)
+        
+        let offset = Int(hash.last! & 0b00001111)
+        
+        var slicedHash = Array(hash[offset...offset+3])
+        
+        slicedHash[0] = slicedHash[0] & 0b01111111
+        
+        let num = Data(slicedHash).withUnsafeBytes { $0.load(as: UInt32.self).bigEndian }
+        
+        let top = String(num).suffix(6)
+        
+        return String(top)
+    }
+}
+
 struct CodeModel {
     
     init(codeItem: CodeItem) {
@@ -39,31 +71,7 @@ struct CodeModel {
     var key : String
     var code: String {
         get {
-            guard let key = base32Decode(key.replacingOccurrences(of: " ", with: "")) else {
-                return "Error!"
-            }
-            
-            let intervalTime = 30
-            let date = Date.now
-            let unixTime: TimeInterval = date.timeIntervalSince1970
-            let unixTimeInt = Int(unixTime)
-            let timeSteps = unixTimeInt / intervalTime
-            
-            let counter = withUnsafeBytes(of: timeSteps.bigEndian, Array.init)
-            
-            let hash = try! HMAC(key: key, variant: .sha1).authenticate(counter)
-            
-            let offset = Int(hash.last! & 0b00001111)
-            
-            var slicedHash = Array(hash[offset...offset+3])
-            
-            slicedHash[0] = slicedHash[0] & 0b01111111
-            
-            let num = Data(slicedHash).withUnsafeBytes { $0.load(as: UInt32.self).bigEndian }
-            
-            let top = String(num).suffix(6)
-            
-            return String(top)
+            OtpCodeModel.computeCode(key: key)
         }
     }
     
